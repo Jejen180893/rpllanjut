e0fd8985a9bc60ef85b3d1bbd4081c333c381246
JejenWahyudin <zezen_gamers@yahoo.com>
2014-09-15 15:25:06 +0700

    ini commit ketiga saya

---
+++ PaketBeraksi.java
@@ -0,0 +1,10 @@
+import kelasku.*;
+public class PaketBeraksi{
+	public static void main(String[] args){
+		Budi objectBudi=new Budi();
+		objectBudi.info();
+		Tono objectTono=new Tono();
+		objectTono.info();
+		}
+	}
+	
